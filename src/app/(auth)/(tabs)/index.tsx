25e280c872ba42c24820d334a37392384fe4c32e
iliapp <iluhasmakovskyi@gmai.com>
2026-09-11 15:58:34 +0200

    implements: fonts

--- src/app/(auth)/(tabs)/index.tsx
+++ src/app/(auth)/(tabs)/index.tsx
@@ -9,35 +9,33 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
 
 
       <Link href="/onboarding" asChild>
-        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+        <Pressable className="mt-4 font-sans-bold  w-full rounded bg-primary p-4">
           <Text className="text-white">Go to Onboarding</Text>
         </Pressable>
       </Link>
       <Link href="/(auth)/sign-in" asChild>
-        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+        <Pressable className="mt-4 font-sans-bold w-full rounded bg-primary p-4">
           <Text className="text-white">Go to Sign In</Text>
         </Pressable>
       </Link>
       <Link href="/(auth)/sign-up" asChild>
-        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+        <Pressable className="mt-4 font-sans-bold w-full rounded bg-primary p-4">
           <Text className="text-white">Go to Sign Up</Text>
         </Pressable>
       </Link>
 
-                  <Link href="/subscriptions/spotify">Go to Subscription(Spotify)</Link>
-                  <Link href={{
-                    pathname: "/subscriptions/[id]",
-                    params: { id: "claude" },
-                  }}
-                  >
-                    Claude Max Subscription
-                  </Link>
+                  {/*<Link href="/subscriptions/spotify">Go to Subscription(Spotify)</Link>*/}
+                  {/*<Link href={{*/}
+                  {/*  pathname: "/subscriptions/[id]",*/}
+                  {/*  params: { id: "claude" },*/}
+                  {/*}}*/}
+                  {/*>*/}
+                  {/*  Claude Max Subscription*/}
+                  {/*</Link>*/}
     </SafeAreaView>
   );
 }
