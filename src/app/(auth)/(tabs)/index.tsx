ce5f6fd2745c079a72291aa94c0c5d9f6f730350
iliapp <iluhasmakovskyi@gmai.com>
2026-09-11 15:09:36 +0200

    feat:project setup,natewind,routing, tab navigation, and bug fix

--- src/app/(auth)/(tabs)/index.tsx
+++ src/app/(auth)/(tabs)/index.tsx
@@ -1,23 +1,34 @@
-import { Link } from "expo-router";
-import { cssInterop } from "nativewind";
-import { Text } from "react-native";
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import "../../global.css";
-
-cssInterop(RNSafeAreaView, { className: "style" });
+import { Link } from "expo-router";
+import { Pressable, Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "@/lib/styled";
+const SafeAreaView = styled(RNSafeAreaView);
 
 
 export default function App() {
   return (
-    <RNSafeAreaView className="flex-1 items-center justify-center bg-background p-5">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
 
 
-      <Link href="/onboarding" className="mt-4 rounded bg-primary p-4 text-white">Go to Onboarding</Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary p-4 text-white">Go to Sign In</Link>
-                  <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary p-4 text-white">Go to Sign Up</Link>
+      <Link href="/onboarding" asChild>
+        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+          <Text className="text-white">Go to Onboarding</Text>
+        </Pressable>
+      </Link>
+      <Link href="/(auth)/sign-in" asChild>
+        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+          <Text className="text-white">Go to Sign In</Text>
+        </Pressable>
+      </Link>
+      <Link href="/(auth)/sign-up" asChild>
+        <Pressable className="mt-4 w-full rounded bg-primary p-4">
+          <Text className="text-white">Go to Sign Up</Text>
+        </Pressable>
+      </Link>
 
                   <Link href="/subscriptions/spotify">Go to Subscription(Spotify)</Link>
                   <Link href={{
@@ -27,6 +38,6 @@ export default function App() {
                   >
                     Claude Max Subscription
                   </Link>
-    </RNSafeAreaView>
+    </SafeAreaView>
   );
 }
